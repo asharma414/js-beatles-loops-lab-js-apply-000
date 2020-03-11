b64145cb04ecb49c7b36efc384f35276c8c5d050
benign-glitch-2757 <abhi.sharm.414@gmail.com>
2020-03-11 02:29:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 // add solution here
+
+function theBeatlesPlay(musicians, instruments) {
+  let arr = [];
+  for (let i = 0; i < musicians.length; i++) {
+    arr.push(musicians[i] + " plays " + instruments[i]);
+  }
+  return arr;
+}
 function iLoveTheBeatles(num) {
   
 }
